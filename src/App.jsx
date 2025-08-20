a073033259f0eebdd415db1a4b0de0d40a809a98
unknown <muhammadsyarruri@gmail.com>
2025-08-20 17:11:13 +0700

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,29 @@
-export default function App() {}
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      {/* <Hero /> */}
+      {/* <About /> */}
+      {/* <Process /> */}
+      {/* <Location /> */}
+      {/* <Mitra /> */}
+    </>
+  );
+}
+
+function Navbar() {
+  return (
+    <header>
+      <h1>HABIBAH.</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Tentang</li>
+          <li>Proses</li>
+          <li>Lokasi</li>
+          <li>Mitra</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
