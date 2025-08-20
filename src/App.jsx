05ed600ca12ba9f4964adf3838e919a3ed1857b6
unknown <muhammadsyarruri@gmail.com>
2025-08-20 19:19:46 +0700

    add about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ export default function App() {
     <>
       <Navbar />
       <Hero />
-      {/* <About /> */}
+      <About />
       {/* <Process /> */}
       {/* <Location /> */}
       {/* <Mitra /> */}
@@ -40,6 +40,28 @@ function Hero() {
         <button>Pesan Sekarang</button>
       </div>
       <img src="/img/heroimg.png" alt="telur asin" />
+    </section>
+  );
+}
+
+function About() {
+  return (
+    <section className="about-section">
+      <h2 className="section-title">Tentang Kami</h2>
+      <p>
+        Di sini, kami menghadirkan telur asin terbaik untuk Anda. Perjalanan
+        kami dimulai pada tahun 2015 dengan misi sederhana yaitu memproduksi
+        telur asin berkualitas tinggi dan lezat menggunakan metode tradisional
+        yang dikombinasikan dengan standar kebersihan dan kontrol kualitas
+        modern.
+      </p>
+      <p>
+        Berlokasi di Palembang, Telur Asin Habibah didirikan oleh Tunai Jaya,
+        yang memiliki apresiasi mendalam terhadap seni membuat telur asin.
+        Terinspirasi oleh resep kuno yang diwariskan turun-temurun, kami memulai
+        pencarian untuk menciptakan telur asin yang sempurna, menyeimbangkan
+        rasa, tekstur, dan nilai gizi.
+      </p>
     </section>
   );
 }
