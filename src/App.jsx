4c8775022a016895a45916d1fe37dd84791f264e
unknown <muhammadsyarruri@gmail.com>
2025-08-22 08:36:11 +0700

    add product section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
       <Hero />
       <About />
       <Process />
+      <Product />
       {/* <Location /> */}
       {/* <Mitra /> */}
     </>
@@ -145,6 +146,27 @@ function Process() {
           ></span>
         ))}
       </div>
+    </section>
+  );
+}
+
+function Product() {
+  return (
+    <section className="product-section">
+      <h2 className="section-title">Produk</h2>
+      <div className="product">
+        <img src="/img/display1.jpeg" alt="telur asin" />
+        <img src="/img/display2.jpg" alt="telur asin" />
+        <img src="/img/display3.jpeg" alt="telur asin" />
+      </div>
+      <p className="price">Harga: Rp3.500 / butir</p>
+      <ul className="note">
+        <strong>Catatan:</strong>
+        <li>Minimal 3 butir untuk layanan antar.</li>
+        <li>Kurang dari 3 butir, ambil di lokasi.</li>
+        <li>Luar kota via ekspedisi (ongkir di tanggung pembeli).</li>
+        <li>telur asin di stok ulang setiap hari selasa dan jum'at.</li>
+      </ul>
     </section>
   );
 }
